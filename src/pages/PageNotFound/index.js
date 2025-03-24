2fa2bb92796801d4f394e0503f53f5937fd247af
niculox <nicolasamaral1997@gmail.com>
2025-03-24 18:37:35 -0300

    Add PageNotFound component and routing; implement 404 error page with styles

--- src/pages/PageNotFound/index.js
+++ src/pages/PageNotFound/index.js
@@ -1,6 +1,20 @@
+import style from "./PageNotFound.module.css";
+import Header from "../../components/Header";
+import Footer from "../../components/Footer";
+import erro404 from "./imgnotfound.png";
+
 function PageNotFound() {
   return (
-    <h1>Page Not Found</h1>
+    <>
+      <Header/>
+      <div className={style.pagenotfound}>
+        
+        <h1>erro 404</h1>
+        <h2>Página não encontrada</h2>
+        <img src={erro404} alt="notfound" class="rotating"/>
+        <Footer />
+      </div>
+    </>
   );
 }
 
--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import MyPage from "./pages/MyPage";
+import PageNotFound from "./pages/PageNotFound";
 
 function AppRoutes(){
     return (
@@ -8,6 +9,7 @@ function AppRoutes(){
             <Routes>
                 <Route path="/" element={ <Home /> }></Route>
                 <Route path="/MyPage" element={ <MyPage /> }></Route>
+                <Route path="*" element={ <PageNotFound /> }></Route>
             </Routes>
         </BrowserRouter>
     );
