e6931f1f8e1173b2a1359858b1b6f97cb73a6fa4
niculox <nicolasamaral1997@gmail.com>
2025-03-24 17:22:30 -0300

    Add additional anime images to Container component

--- src/components/Container/index.js
+++ src/components/Container/index.js
@@ -8,6 +8,31 @@ import kisuke from "../../assets/kisuke.jpg";
 import rukia from "../../assets/rukia.jpg";
 import madara from "../../assets/madara.jpg";
 import totoro from "../../assets/totoro.jpg";
+import Aki from "../../assets/Aki.jpg";
+import animeimg from "../../assets/animeimg.jpg";
+import background from "../../assets/background.jpg";
+import bankaiunohana from "../../assets/bankaiunohana.jpg";
+import casteloghibli from "../../assets/casteloghibli.jpg";
+import fantasmaghibli from "../../assets/fantasmaghibli.jpg";
+import gatokiki from "../../assets/gatokiki.jpg";
+import gatonolago from "../../assets/gatonolago.jpg";
+import jujutsu from "../../assets/jujutsu.jpg";
+import kakashihatake from "../../assets/kakashihatake.jpg";
+import kiki from "../../assets/kiki.jpg";
+import kimetsu from "../../assets/kimetsu.jpg";
+import kurosakiichigo from "../../assets/kurosakiichigo.jpg";
+import makima from "../../assets/makima.jpg";
+import narutoasthetic from "../../assets/narutoaesthetic.jpg";
+import narutoart from "../../assets/narutoart.jpg";
+import narutocostas from "../../assets/narutocostas.jpg";
+import narutowallpapper from "../../assets/narutowallpaper.jpg";
+import princessmononoke from "../../assets/princessmononoke.jpg";
+import princessmononokepink from "../../assets/princessmononokepink.jpg";
+import shinjibleach from "../../assets/shinjibleach.jpg";
+import studioghibliaesthetic from "../../assets/studioghibliaesthetic.jpg";
+import totorochuva from "../../assets/totorochuva.jpg";
+import yhwa from "../../assets/yhwa.jpg";
+import yourname from "../../assets/yourname.jpg";
 
 function Container(){
     return (
@@ -34,7 +59,32 @@ const imgContainer = [
     kisuke,
     rukia,
     madara,
-    totoro
+    totoro,
+    Aki,
+    animeimg,
+    background,
+    bankaiunohana,
+    casteloghibli,
+    fantasmaghibli,
+    gatokiki,
+    gatonolago,
+    jujutsu,
+    kakashihatake,
+    kiki,
+    kimetsu,
+    kurosakiichigo,
+    makima,
+    narutoasthetic,
+    narutoart,
+    narutocostas,
+    narutowallpapper,
+    princessmononoke,
+    princessmononokepink,
+    shinjibleach,
+    studioghibliaesthetic,
+    totorochuva,
+    yhwa,
+    yourname
 ];
 
 //função que sorteia uma imagem do array imgContainer
