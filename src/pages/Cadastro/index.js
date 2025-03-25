30a3701e64afe2b36a212dcdecdef5d1dd9e3098
niculox <nicolasamaral1997@gmail.com>
2025-03-25 17:40:15 -0300

    Refactor Cadastro and Login pages for improved layout and user experience; update styles and add navigation links

--- src/pages/Cadastro/index.js
+++ src/pages/Cadastro/index.js
@@ -1,21 +1,25 @@
+import { Link } from 'react-router-dom';
 import imagem from "./imgcadastro.jpg";
 import style from "./Cadastro.module.css";
+
 function Cadastro(){
     return (
-        <div className={style.cadastro}>
-            <form>
-                <h1>Olá! Fico feliz em te ver aqui!</h1>
-                <h2>Preencha os campos para realizar seu cadastro.</h2>
-                <label for="username">Username:</label>
-                <input type="text" id="username" name="username" required/>
-                <label for="email">E-mail:</label>
-                <input type="text" id="email" name="email" required/>
-                <label for="senha">Senha:</label>
-                <input type="text" id="senha" name="senha" required/>
-                <input type="submit" value="Cadastrar"/>
-            </form>
-            <img src={imagem} alt="background-login"/>
-        </div>
+        <section className={style.cadastro}>
+            <div>
+                <form>
+                    <h1>Olá! Fico feliz em te ver aqui!</h1>
+                    <h2>Já tem cadastro? Realize o login: <Link to="/Login">Login</Link></h2>
+                    <label for="username">Username:</label>
+                    <input type="text" id="username" name="username" required/>
+                    <label for="email">E-mail:</label>
+                    <input type="text" id="email" name="email" required/>
+                    <label for="senha">Senha:</label>
+                    <input type="text" id="senha" name="senha" required/>
+                    <button id="cadastrar" value="cadastrar">Cadastrar</button>
+                </form>
+                <img src={imagem} alt="background-login"/>
+            </div>
+        </section>
     );
 }
 
--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,19 +1,23 @@
+import { Link } from 'react-router-dom';
 import imagem from "./imglogin.jpg";
 import style from "./Login.module.css";
 
 function Login(){
     return (
-        <div className={ style.login }>
-            <img src={imagem} alt="background-login"/>
-            <form>
-                <h1>Bem vindo de volta!</h1>
-                <label for="username">Username:</label>
-                <input type="text" id="username" name="username" required/>
-                <label for="senha">Senha:</label>
-                <input type="text" id="senha" name="senha" required/>
-                <button id="logar" value="Entrar">Entrar</button>
-            </form>
-        </div>
+        <section className={ style.login }>
+            <div>
+                <img src={imagem} alt="background-login"/>
+                <form>
+                    <h1>Bem vindo de volta!</h1>
+                    <h2>Ainda não tem cadastro? Realize-o aqui: <Link to="/Cadastro">Cadastro</Link></h2>
+                    <label for="username">Username:</label>
+                    <input type="text" id="username" name="username" required/>
+                    <label for="senha">Senha:</label>
+                    <input type="text" id="senha" name="senha" required/>
+                    <button id="logar" value="Entrar">Entrar</button>
+                </form>
+            </div>
+        </section>
     );
 }
 
