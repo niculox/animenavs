819303f9d29efabb827e2df6a720d5ae7c55029c
niculox <nicolasamaral1997@gmail.com>
2025-03-25 10:44:34 -0300

    Add SectionRec component and update MyPage with profile image and text

---
+++ src/components/SectionRec/index.js
@@ -0,0 +1,7 @@
+function SectionRec(){
+    return (
+        <h1></h1>
+    );
+}
+
+export default SectionRec;
--- src/pages/MyPage/index.js
+++ src/pages/MyPage/index.js
@@ -2,6 +2,7 @@ import Header from "../../components/Header";
 import Footer from "../../components/Footer";
 import Banner from "../../components/Banner"
 import style from "./MyPage.module.css";
+import foto from "./niculos.png";
 
 function MyPage() {
   return (
@@ -9,13 +10,14 @@ function MyPage() {
         <Header />
         <Banner />
           <div className={style.mypage}>
-            
+            <img src={ foto } alt="foto-perfil-niculos" />
+            <h1>niculos</h1>
+            <h2>Recomendações salvas</h2>
+            <p>"Recomendações"</p>
           </div>
         <Footer />
       </>
   );
 }
-
-
 
 export default MyPage;
