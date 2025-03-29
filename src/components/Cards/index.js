2aa3f16a529c53ed8ab1b1c4e992a37cbe90ca58
niculox <nicolasamaral1997@gmail.com>
2025-03-29 14:04:08 -0300

    Enhance Cards component with title overlay on hover; refactor styles for better layout and responsiveness. Update Header styles to improve z-index for better visibility.

--- src/components/Cards/index.js
+++ src/components/Cards/index.js
@@ -1,21 +1,39 @@
 import { Link } from "react-router-dom";
+import { useState } from "react"; // Importando useState
 import cards from "../../json/animes.json";
 import style from "./Cards.module.css";
 
+function Cards() {
+    const [titulo, setTitulo] = useState(""); // Estado para armazenar o título
 
-function Cards(){
+    const mostrarTitulo = (titulo) => {
+        setTitulo(titulo); // Define o título quando o mouse está sobre a imagem
+    };
+
+    const esconderTitulo = () => {
+        setTitulo(""); // Limpa o título quando o mouse sai da imagem
+    };
+
     return (
         <section className={style.card}>
             {cards.map((card) => (
-                <Link to={`Anime/${card.id}`}>
-                    <img key={card.id} src={card.imagem} alt={card.titulo}/>
-                    
+                <Link to={`Anime/${card.id}`} key={card.id}>
+                    <div 
+                        className={style.cardContainer} // Adicionando uma classe para o contêiner
+                        onMouseOver={() => mostrarTitulo(card.titulo)} 
+                        onMouseOut={esconderTitulo}
+                    >
+                        <img src={card.imagem} alt={card.titulo} />
+                        {titulo === card.titulo && (
+                            <div className={style.titulo}>
+                                {card.titulo}
+                            </div>
+                        )}
+                    </div>
                 </Link>
             ))}
         </section>
     );
 }
-
-//function mostrarTitulo(id){}
 
 export default Cards;
