8e161ecb82b9e2930122fc0f3f6d5824606710bb
niculox <nicolasamaral1997@gmail.com>
2025-04-01 19:54:27 -0300

    Enhance Footer component styling and content; add text alignment, font properties, and developer attribution.

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -3,7 +3,7 @@ import styles from './Footer.module.css';
 function Footer() {
     return (
         <footer className={styles.footer}>
-
+          <h3>Desenvoolvido por Nícolas Amaral | @niculos_</h3>
         </footer>
     );
   }
