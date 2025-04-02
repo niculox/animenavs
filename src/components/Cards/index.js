d81ed4d33c501868f2b7772fcfe63925e0aed9df
niculox <nicolasamaral1997@gmail.com>
2025-04-02 13:51:30 -0300

    Add Favorites section and enhance Cards component; implement favorite icons and update layout for improved user experience.

--- src/components/Cards/index.js
+++ src/components/Cards/index.js
@@ -2,6 +2,8 @@ import { Link } from "react-router-dom";
 import { useState } from "react"; // Importando useState
 import cards from "../../json/animes.json";
 import style from "./Cards.module.css";
+import favorito from "../../assets/iconfav/2.png";
+import ufavorito from "../../assets/iconfav/1.png";
 
 function Cards() {
     const [titulo, setTitulo] = useState(""); // Estado para armazenar o título
@@ -15,24 +17,27 @@ function Cards() {
     };
 
     return (
-        <section className={style.card}>
-            {cards.map((card) => (
-                <Link to={`Anime/${card.id}`} key={card.id}>
-                    <div 
-                        className={style.cardContainer} // Adicionando uma classe para o contêiner
-                        onMouseOver={() => mostrarTitulo(card.titulo)} 
-                        onMouseOut={esconderTitulo}
-                    >
-                        <img src={card.imagem} alt={card.titulo} />
-                        {titulo === card.titulo && (
-                            <div className={style.titulo}>
-                                {card.titulo}
-                            </div>
-                        )}
-                    </div>
-                </Link>
-            ))}
-        </section>
+            <section className={style.card}>
+                {cards.map((card) => (
+                    <Link to={`Anime/${card.id}`} key={card.id}>
+                        <div 
+                            className={style.cardContainer} // Adicionando uma classe para o contêiner
+                            onMouseOver={() => mostrarTitulo(card.titulo)} 
+                            onMouseOut={esconderTitulo}
+                        >
+                            <img src={card.imagem} alt={card.titulo} />
+                            {titulo === card.titulo && (
+                                <div className={style.titulo}>
+                                    {card.titulo}
+                                </div>
+                            )}
+                            <figure className={style.fav}>
+                                <img src={ufavorito} alt="coração-favorito"/>
+                            </figure>
+                        </div>
+                    </Link>
+                ))}
+            </section>
     );
 }
 
--- src/components/Favorites/index.js
+++ src/components/Favorites/index.js
@@ -1,9 +1,13 @@
+import styles from "./Favorites.module.css";
 
 function Favorites(){
     return (
-        <>
-            
-        </>
+        <section className={styles.favorites}>
+            <h1>Favoritos</h1>
+            <div>
+                
+            </div>
+        </section>
     );
 }
 
