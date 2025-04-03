aa0eb3359e402e56071b483e6c46b3f0f0d6a5ba
niculox <nicolasamaral1997@gmail.com>
2025-04-02 22:10:44 -0300

    Enhance Favorites component; update styling for better layout and add link functionality for favorite items.

--- src/components/Favorites/index.js
+++ src/components/Favorites/index.js
@@ -1,5 +1,6 @@
 import { useFavoriteContext } from "../../contexts/Favorito";
 import styles from "./Favorites.module.css";
+import { Link } from "react-router-dom";
 
 function Favorites() {
     const { favorite } = useFavoriteContext(); // Acessando a lista de favoritos
@@ -9,9 +10,9 @@ function Favorites() {
         <section className={styles.favorites}>
             {favorite.length > 0 ? (
                 favorite.map((item) => (
-                    <div key={item.id}>
-                        <img src={item.imagem} alt={item.titulo} />
-                    </div>
+                        <Link to={`/Anime/${item.id}`} key={item.id} className={styles.card}>
+                            <img src={item.imagem} alt={item.titulo} />
+                        </Link>
                 ))
             ) : (
                 <p>Nenhum item favoritado ainda.</p>
